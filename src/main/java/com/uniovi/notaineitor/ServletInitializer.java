ee34c84a052137d4ddf721f18737199df32e155d
Koquda <uo282874@uniovi.es>
2023-02-08 18:10:30 +0100

    SDI-2223-603-2.2-Trabajando con Controladores

--- src/main/java/com/uniovi/notaineitor/ServletInitializer.java
+++ src/main/java/com/uniovi/notaineitor/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Sdi2223603SpringApplication.class);
+        return application.sources(NotaneitorApplication.class);
     }
 
 }
