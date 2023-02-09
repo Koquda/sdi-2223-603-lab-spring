2f50dc0be9a43ecc19fd754700e73d3683ecec4d
Koquda <uo282874@uniovi.es>
2023-02-09 11:56:41 +0100

    SDI-2223-603-2.4-Modelo, acceso a datos simple

---
+++ src/main/java/com/uniovi/notaineitor/repositories/MarksRepository.java
@@ -0,0 +1,8 @@
+package com.uniovi.notaineitor.repositories;
+
+import com.uniovi.notaineitor.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+
+}
